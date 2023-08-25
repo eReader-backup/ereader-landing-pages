b492fbd08ae4062097c10b3595690d9466c8537d
compro-shridhi <shridhi.aggarwal@comprotechnologies.com>
2023-08-25 17:47:40 +0530

    selected DIV CODE UPDATED

--- script/index.js
+++ script/index.js
@@ -1,9 +1,9 @@
 var inputField = document.getElementById("username");
 var username = "";
 
-inputField.addEventListener("input", function () {
-    username = inputField.value;
-});
+// inputField.addEventListener("input", function () {
+//     username = inputField.value;
+// });
 
 function flipbookClick() {
     let name = username.trim();
@@ -30,28 +30,11 @@ function webbookClick() {
 let lastSelectedDiv = document.getElementById("div1");
 
 function handleDivClick(event) {
-    // Remove the "selected" class from the last selected div (if any)
     if (lastSelectedDiv !== null) {
-        lastSelectedDiv.classList.remove("selected");
+        lastSelectedDiv.classList.remove("selectedDiv");
     }
-
-    // Add the "selected" class to the clicked div
-    event.target.classList.add("selected");
-
-    // Update the last selected div
-    lastSelectedDiv = event.target;
-
-    // Perform your action based on which div was clicked
-    if (event.target.id === "div1") {
-        // Code to perform for Div 1
-        console.log("Div 1 was clicked");
-    } else if (event.target.id === "div2") {
-        // Code to perform for Div 2
-        console.log("Div 2 was clicked");
-    } else if (event.target.id === "div3") {
-        // Code to perform for Div 3
-        console.log("Div 3 was clicked");
-    }
+    event.currentTarget.classList.add("selectedDiv");
+    lastSelectedDiv = event.currentTarget;
 }
 
 document.getElementById("div1").addEventListener("click", handleDivClick);
