57c17038998b259973b70094d5bfeff8eb1d7ad1
compro-shridhi <shridhi.aggarwal@comprotechnologies.com>
2023-08-25 18:14:50 +0530

    add tab, update button click

--- script/index.js
+++ script/index.js
@@ -6,42 +6,40 @@ var username = "";
 // });
 
 function flipbookClick() {
-    let name = username.trim();
-    if (name !== "" && name !== undefined) {
-        let url = "https://wayside-reader1.comprodls.com/demo/student-edition/interkulturel_1_vtext?username=" + name;
+    // let name = username.trim();
+    // if (name !== "" && name !== undefined) {
+        let url = "https://wayside-reader1.comprodls.com/demo/student-edition/interkulturel_1_vtext";
+        // ?username=" + name;
         window.open(url, "_blank");
-    } else {
-        username = "";
-        alert("Enter your name to launch the Reader.")
-    }
+    // } else {
+    //     username = "";
+    //     alert("Enter your name to launch the Reader.")
+    // }
 }
 
 function webbookClick() {
-    let name = username.trim();
-    if (name !== "" && name !== undefined) {
-        let url = "https://wayside-reader2.comprodls.com/demo/student-edition/interkulturel_1_vtext?username=" + name;
+    // let name = username.trim();
+    // if (name !== "" && name !== undefined) {
+        let url = "https://wayside-reader2.comprodls.com/demo/student-edition/interkulturel_1_vtext";
+        // ?username="+ name;
         window.open(url, "_blank");
-    } else {
-        username = "";
-        alert("Enter your name to launch the Reader.")
-    }
+    // } else {
+    //     username = "";
+    //     alert("Enter your name to launch the Reader.")
+    // }
 }
 
 let lastSelectedDiv = document.getElementById("div1");
 let lastSelectedButton = document.getElementById("div1").querySelector("button");
 
 function handleDivClick(event) {
-    debugger
     if (lastSelectedDiv !== null) {
         lastSelectedDiv.classList.remove("selectedDiv");
     }
     event.currentTarget.classList.add("selectedDiv");
     lastSelectedDiv = event.currentTarget;
 
-    // Get the button element within the div
     const button = event.currentTarget.querySelector("button");
-
-    // Apply different styles to the button in the selected div
     if (button) {
         if (lastSelectedButton !== null) {
             lastSelectedButton.classList.remove("launchSelectedButton");
