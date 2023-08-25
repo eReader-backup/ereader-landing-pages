cbed147001f18f1f6d9c3f0a683b2a2f60b37500
compro-shridhi <shridhi.aggarwal@comprotechnologies.com>
2023-08-25 18:02:26 +0530

    selected button css updated

--- script/index.js
+++ script/index.js
@@ -28,13 +28,27 @@ function webbookClick() {
 }
 
 let lastSelectedDiv = document.getElementById("div1");
+let lastSelectedButton = document.getElementById("div1").querySelector("button");
 
 function handleDivClick(event) {
+    debugger
     if (lastSelectedDiv !== null) {
         lastSelectedDiv.classList.remove("selectedDiv");
     }
     event.currentTarget.classList.add("selectedDiv");
     lastSelectedDiv = event.currentTarget;
+
+    // Get the button element within the div
+    const button = event.currentTarget.querySelector("button");
+
+    // Apply different styles to the button in the selected div
+    if (button) {
+        if (lastSelectedButton !== null) {
+            lastSelectedButton.classList.remove("launchSelectedButton");
+        }
+        button.classList.add("launchSelectedButton");
+        lastSelectedButton = button;
+    }
 }
 
 document.getElementById("div1").addEventListener("click", handleDivClick);
