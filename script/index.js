c7b4f316150757fd00fb659e2321c7cefd39a871
compro-shridhi <shridhi.aggarwal@comprotechnologies.com>
2023-08-25 17:34:15 +0530

    update launch button and grid item css

--- script/index.js
+++ script/index.js
@@ -26,3 +26,34 @@ function webbookClick() {
         alert("Enter your name to launch the Reader.")
     }
 }
+
+let lastSelectedDiv = document.getElementById("div1");
+
+function handleDivClick(event) {
+    // Remove the "selected" class from the last selected div (if any)
+    if (lastSelectedDiv !== null) {
+        lastSelectedDiv.classList.remove("selected");
+    }
+
+    // Add the "selected" class to the clicked div
+    event.target.classList.add("selected");
+
+    // Update the last selected div
+    lastSelectedDiv = event.target;
+
+    // Perform your action based on which div was clicked
+    if (event.target.id === "div1") {
+        // Code to perform for Div 1
+        console.log("Div 1 was clicked");
+    } else if (event.target.id === "div2") {
+        // Code to perform for Div 2
+        console.log("Div 2 was clicked");
+    } else if (event.target.id === "div3") {
+        // Code to perform for Div 3
+        console.log("Div 3 was clicked");
+    }
+}
+
+document.getElementById("div1").addEventListener("click", handleDivClick);
+document.getElementById("div2").addEventListener("click", handleDivClick);
+document.getElementById("div3").addEventListener("click", handleDivClick);
