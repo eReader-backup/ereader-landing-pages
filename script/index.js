caea743504c908b43118b887cbcd6d9ffc99669a
Jasdeep Singh <jasdeep.singh@comprotechnologies.com>
2023-08-28 16:34:45 +0530

    ,minor updates

--- script/index.js
+++ script/index.js
@@ -1,54 +1,47 @@
 var inputField = document.getElementById("username");
 var username = "";
 
-// inputField.addEventListener("input", function () {
-//     username = inputField.value;
-// });
-
 function flipbookClick() {
-    // let name = username.trim();
-    // if (name !== "" && name !== undefined) {
-        let url = "https://wayside-reader1.comprodls.com/demo/student-edition/interkulturel_1_vtext";
-        // ?username=" + name;
-        window.open(url, "_blank");
-    // } else {
-    //     username = "";
-    //     alert("Enter your name to launch the Reader.")
-    // }
+  let url =
+    "https://wayside-reader1.comprodls.com/demo/student-edition/interkulturel_1_vtext";
+  window.open(url, "_blank");
 }
 
 function webbookClick() {
-    // let name = username.trim();
-    // if (name !== "" && name !== undefined) {
-        let url = "https://wayside-reader2.comprodls.com/demo/student-edition/interkulturel_1_vtext";
-        // ?username="+ name;
-        window.open(url, "_blank");
-    // } else {
-    //     username = "";
-    //     alert("Enter your name to launch the Reader.")
-    // }
+  let url =
+    "https://wayside-reader2.comprodls.com/demo/student-edition/interkulturel_1_vtext";
+  window.open(url, "_blank");
+}
+
+function flipbookClickTE() {
+  let url =
+    "https://wayside-reader1.comprodls.com/demo/teacher-edition/interkulturel_1_te";
+  window.open(url, "_blank");
 }
 
 let lastSelectedDiv = document.getElementById("div1");
-let lastSelectedButton = document.getElementById("div1").querySelector("button");
+let lastSelectedButtons = [
+  document.getElementById("div1").querySelector("button"),
+];
 
 function handleDivClick(event) {
+  if (event.target.tagName != "BUTTON" && lastSelectedDiv.id != event.currentTarget.id) {
     if (lastSelectedDiv !== null) {
-        lastSelectedDiv.classList.remove("selectedDiv");
+      lastSelectedDiv.classList.remove("selectedDiv");
     }
     event.currentTarget.classList.add("selectedDiv");
     lastSelectedDiv = event.currentTarget;
 
-    // const button = event.currentTarget.querySelector("button");
-    // if (button) {
-    //     if (lastSelectedButton !== null) {
-    //         lastSelectedButton.classList.remove("launchSelectedButton");
-    //     }
-    //     button.classList.add("launchSelectedButton");
-    //     lastSelectedButton = button;
-    // }
+    const buttons = event.currentTarget.querySelectorAll("button");
+    buttons.forEach((button) => {
+      lastSelectedButtons.forEach((selectedbutton) => {
+        selectedbutton.classList.remove("launchSelectedButton");
+      });
+      button.classList.add("launchSelectedButton");
+    });
+    lastSelectedButtons = buttons;
+  }
 }
 
 document.getElementById("div1").addEventListener("click", handleDivClick);
 document.getElementById("div2").addEventListener("click", handleDivClick);
-document.getElementById("div3").addEventListener("click", handleDivClick);
